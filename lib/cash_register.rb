959f104deb4ffce8efe25198d652422a6bbd6349
s.ttrout@hotmail.com <sashin123@LAPTOP-D36VJ7HR.localdomain>
2020-10-14 13:59:16 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,10 @@
+class CashRegister 
+    #setter and getter methods
+    attr_accessor :totoal, :discount, :items, :last_item_price
 
+    #initialize discount with a default value of 0
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount
+        @items = []
+end
